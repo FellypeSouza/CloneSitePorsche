ed371a4c5b01f8549263fcb3d03392f5b9d4611d
FellypeSouza <fellypedev23@gmail.com>
2024-05-09 16:31:29 -0300

    commit v0.0.2

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,38 @@ export default function Header(){
     return(
         <section>
             <img src={imageheader} className="img-fluid w-100" alt='porsche' id='imageHeader'/>
-            <section className='headerTitle'>
-                <h1>Seminovos</h1>
+
+            <section className="mainContainer">{/* container principal */}
+
+                <section className='headerContent'>{/* lado esquerdo */}
+                    <h1 className="headerTitle">Seminovos</h1>
+                    <h5>Encontre mais fácil aqui</h5>
+                    <input placeholder="Palavra-chave" className="input"/>
+                </section>
+
+                <section className="headerContentRight">{/* lado direito */}
+                    <section className='models'>
+                        <section className='model'>
+                            <img src="https://image.webmotors.com.br/vmotors-images/2024/5/7/3585ad67-f1a3-4e5f-8551-c40936cebef0.webp" width="355" height="265"/>
+                            <figcaption className='m-3'><b>CAYENNE 3.0 V6 E-HYBRID COUPÉ AWD TIPTRONIC S</b></figcaption>
+                            <section className='d-flex justify-content-between p-3'>
+                                <p>Por</p> 
+                                <h4>R$745.000,00</h4>
+                            </section>
+                            <hr className='modelCross p-1'/>
+                            <section className='p-3'>
+                                <figcaption><b>Km:</b> 8511</figcaption>
+                                <figcaption><b>Cor:</b> Branco</figcaption>
+                                <figcaption><b>Câmbio:</b> Automático</figcaption>
+                                <figcaption><b>Unidade:</b> Curitiba - PR</figcaption>
+                            </section>
+                        </section>
+                    </section>
+                    
+                </section>
+
             </section>
-            <section className='container'>
-                <h1>headerContent</h1>
-            </section>
+
         </section>
     );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,20 @@
 import React from "react";
+import '../styles/nav.css';
 import porschelogo from '../styles/images/porsche-logo.png';
 
 export default function Nav(){
     return(
-        <section className="nav nav-fill">
-            <button className="nav-item btn m-4">
+        <section className="navContainer d-flex justify-content-between m-auto">
+            <button className="nav-item btn m-3">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                 </svg>
                 Menu
             </button>
-            <a href="#" className="nav-item m-4">
+            <a href="#" className="nav-item m-3">
                 <img src={porschelogo} width="150" alt="Logo Porsche" title="Logo Porsche"/>
             </a>
-            <a href="#" className="nav-item m-4">
+            <a href="#" className="nav-item m-3">
                 <img src="https://www.stuttgartporsche.com.br/img/logo_stuttgart.svg" width="100" height="28" alt="Stuttgart Porsche" title="Stuttgart Porsche"/>
             </a>
         </section>
