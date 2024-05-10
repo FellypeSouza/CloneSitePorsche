50d7f5e6b402b35c319ba32ce15ba8e7bbd000ff
FellypeSouza <fellypedev23@gmail.com>
2024-05-10 17:36:09 -0300

    commit v0.0.2

---
+++ src/components/CarModels.js
@@ -0,0 +1,143 @@
+import React from "react";
+import imageCar1 from '../styles/images/image-car1.webp';
+import '../styles/CarModels.css';
+
+export default function CarModels(){
+    return(
+        <section className='models'>
+            <section className='model'>
+                <img src={imageCar1} width="355" height="265"/>
+                <figcaption className='m-3'><b>CAYENNE 3.0 V6 E-HYBRID COUPÉ AWD TIPTRONIC S</b></figcaption>
+                <section className='d-flex justify-content-between p-3'>
+                    <p>Por</p> 
+                    <h4>R$745.000,00</h4>
+                </section>
+                <hr className='modelCross m-1 border-2 border-dark'/>
+                <section className='p-3'>
+                    <figcaption><b>Km:</b> 8511</figcaption>
+                    <figcaption><b>Cor:</b> Branco</figcaption>
+                    <figcaption><b>Câmbio:</b> Automático</figcaption>
+                    <figcaption><b>Unidade:</b> Curitiba - PR</figcaption>
+                </section>
+                <section className="openModel p-3">
+                    <h5 className="text-white fs-6">Quero essa oferta</h5>
+                                
+                </section>
+            </section>
+            <section className='model'>
+                <img src={imageCar1} width="355" height="265"/>
+                <figcaption className='m-3'><b>CAYENNE 3.0 V6 E-HYBRID COUPÉ AWD TIPTRONIC S</b></figcaption>
+                <section className='d-flex justify-content-between p-3'>
+                    <p>Por</p> 
+                    <h4>R$745.000,00</h4>
+                </section>
+                <hr className='modelCross m-1 border-2 border-dark'/>
+                <section className='p-3'>
+                    <figcaption><b>Km:</b> 8511</figcaption>
+                    <figcaption><b>Cor:</b> Branco</figcaption>
+                    <figcaption><b>Câmbio:</b> Automático</figcaption>
+                    <figcaption><b>Unidade:</b> Curitiba - PR</figcaption>
+                </section>
+                <section className="openModel p-3">
+                    <h5 className="text-white fs-6">Quero essa oferta</h5>
+                                
+                </section>
+            </section>
+            <section className='model'>
+                <img src={imageCar1} width="355" height="265"/>
+                <figcaption className='m-3'><b>CAYENNE 3.0 V6 E-HYBRID COUPÉ AWD TIPTRONIC S</b></figcaption>
+                <section className='d-flex justify-content-between p-3'>
+                    <p>Por</p> 
+                    <h4>R$745.000,00</h4>
+                </section>
+                <hr className='modelCross m-1 border-2 border-dark'/>
+                <section className='p-3'>
+                    <figcaption><b>Km:</b> 8511</figcaption>
+                    <figcaption><b>Cor:</b> Branco</figcaption>
+                    <figcaption><b>Câmbio:</b> Automático</figcaption>
+                    <figcaption><b>Unidade:</b> Curitiba - PR</figcaption>
+                </section>
+                <section className="openModel p-3">
+                    <h5 className="text-white fs-6">Quero essa oferta</h5>
+                                
+                </section>
+            </section>
+            <section className='model'>
+                <img src={imageCar1} width="355" height="265"/>
+                <figcaption className='m-3'><b>CAYENNE 3.0 V6 E-HYBRID COUPÉ AWD TIPTRONIC S</b></figcaption>
+                <section className='d-flex justify-content-between p-3'>
+                    <p>Por</p> 
+                    <h4>R$745.000,00</h4>
+                </section>
+                <hr className='modelCross m-1 border-2 border-dark'/>
+                <section className='p-3'>
+                    <figcaption><b>Km:</b> 8511</figcaption>
+                    <figcaption><b>Cor:</b> Branco</figcaption>
+                    <figcaption><b>Câmbio:</b> Automático</figcaption>
+                    <figcaption><b>Unidade:</b> Curitiba - PR</figcaption>
+                </section>
+                <section className="openModel p-3">
+                    <h5 className="text-white fs-6">Quero essa oferta</h5>
+                                
+                </section>
+            </section>
+            <section className='model'>
+                <img src={imageCar1} width="355" height="265"/>
+                <figcaption className='m-3'><b>CAYENNE 3.0 V6 E-HYBRID COUPÉ AWD TIPTRONIC S</b></figcaption>
+                <section className='d-flex justify-content-between p-3'>
+                    <p>Por</p> 
+                    <h4>R$745.000,00</h4>
+                </section>
+                <hr className='modelCross m-1 border-2 border-dark'/>
+                <section className='p-3'>
+                    <figcaption><b>Km:</b> 8511</figcaption>
+                    <figcaption><b>Cor:</b> Branco</figcaption>
+                    <figcaption><b>Câmbio:</b> Automático</figcaption>
+                    <figcaption><b>Unidade:</b> Curitiba - PR</figcaption>
+                </section>
+                <section className="openModel p-3">
+                    <h5 className="text-white fs-6">Quero essa oferta</h5>
+                                
+                </section>
+            </section>
+            <section className='model'>
+                <img src={imageCar1} width="355" height="265"/>
+                <figcaption className='m-3'><b>CAYENNE 3.0 V6 E-HYBRID COUPÉ AWD TIPTRONIC S</b></figcaption>
+                <section className='d-flex justify-content-between p-3'>
+                    <p>Por</p> 
+                    <h4>R$745.000,00</h4>
+                </section>
+                <hr className='modelCross m-1 border-2 border-dark'/>
+                <section className='p-3'>
+                    <figcaption><b>Km:</b> 8511</figcaption>
+                    <figcaption><b>Cor:</b> Branco</figcaption>
+                    <figcaption><b>Câmbio:</b> Automático</figcaption>
+                    <figcaption><b>Unidade:</b> Curitiba - PR</figcaption>
+                </section>
+                <section className="openModel p-3">
+                    <h5 className="text-white fs-6">Quero essa oferta</h5>
+                                
+                </section>
+            </section>
+            <section className='model'>
+                <img src={imageCar1} width="355" height="265"/>
+                <figcaption className='m-3'><b>CAYENNE 3.0 V6 E-HYBRID COUPÉ AWD TIPTRONIC S</b></figcaption>
+                <section className='d-flex justify-content-between p-3'>
+                    <p>Por</p> 
+                    <h4>R$745.000,00</h4>
+                </section>
+                <hr className='modelCross m-1 border-2 border-dark'/>
+                <section className='p-3'>
+                    <figcaption><b>Km:</b> 8511</figcaption>
+                    <figcaption><b>Cor:</b> Branco</figcaption>
+                    <figcaption><b>Câmbio:</b> Automático</figcaption>
+                    <figcaption><b>Unidade:</b> Curitiba - PR</figcaption>
+                </section>
+                <section className="openModel p-3">
+                    <h5 className="text-white fs-6">Quero essa oferta</h5>
+                                
+                </section>
+            </section>
+        </section>
+    );
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,26 @@
 import imageheader from '../styles/images/image-header.jpg';
 import '../styles/images/header.css';
-import imageCar1 from '..styles/images/image-car1.webp';
+
+import CarModels from './CarModels';
 
 export default function Header(){
     return(
         <section>
             <img src={imageheader} className="img-fluid w-100" alt='porsche' id='imageHeader'/>
 
-            <section className="mainContainer">{/* container principal */}
-
+            <section className="mainContainer p-5">{/* container principal */}
                 <section className='headerContent'>{/* lado esquerdo */}
-                    <h1 className="headerTitle">Seminovos</h1>
                     <h5>Encontre mais fácil aqui</h5>
                     <input placeholder="Palavra-chave" className="input"/>
+                    <hr className='modelCross mt-4 border-2 border-dark'/>
+                    <input type='range' className='inputRange w-75 m-4'/>
+                    <input type='range' className='inputRange w-75 m-4'/>
                 </section>
 
                 <section className="headerContentRight">{/* lado direito */}
-                    <section className='models'>
-                        <section className='model'>
-                            <img src={imageCar1} width="355" height="265"/>
-                            <figcaption className='m-3'><b>CAYENNE 3.0 V6 E-HYBRID COUPÉ AWD TIPTRONIC S</b></figcaption>
-                            <section className='d-flex justify-content-between p-3'>
-                                <p>Por</p> 
-                                <h4>R$745.000,00</h4>
-                            </section>
-                            <hr className='modelCross p-1'/>
-                            <section className='p-3'>
-                                <figcaption><b>Km:</b> 8511</figcaption>
-                                <figcaption><b>Cor:</b> Branco</figcaption>
-                                <figcaption><b>Câmbio:</b> Automático</figcaption>
-                                <figcaption><b>Unidade:</b> Curitiba - PR</figcaption>
-                            </section>
-                        </section>
-                    </section>
-                    
+                    <CarModels/>
                 </section>
-
+                <br></br>
             </section>
 
         </section>
