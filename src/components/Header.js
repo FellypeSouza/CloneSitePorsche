26ac255ff15961e561cf2cf7e0686e624a47f7c6
FellypeSouza <fellypedev23@gmail.com>
2024-06-02 20:03:37 -0300

    commit v0.0.2

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,13 +26,65 @@ export default function Header(){
                             <p>70000 Máx.</p>
                         </section>
                     </section>
-                    <p>Unidade</p>
-                    <input list='unidade' placeholder="Selecione uma Unidade" className="input w-75"/>
-                    <datalist id='unidade'>
-                        <option value="ola"/>
-                        <option value="ola"/>
-                        <option value="ola"/>
-                    </datalist>
+                    <p className='m-1'>Unidade</p>
+                    <select class="form-select input w-75 border-1 border-dark rounded-0 mb-4" id="inputGroupSelect01">
+                        <option selected>Selecione uma Unidade</option>
+                        <option value="1">São Paulo - SP (Matriz)</option>
+                        <option value="2">Campinas - SP</option>
+                        <option value="3">Ribeirão Preto - SP</option>
+                        <option value="4">Rio de Janeiro - RJ</option>
+                        <option value="5">Porto Alegre - RS</option>
+                        <option value="6">Curitiba - PR</option>
+                        <option value="7">Florianópolis - SC</option>
+                        <option value="8">Curitiba - PR</option>
+                    </select>
+                    <p className='m-1'>Marca</p>
+                    <select class="form-select input w-75 border-1 border-dark rounded-0 mb-4" id="inputGroupSelect02">
+                        <option selected>Selecione uma Marca</option>
+                        <option value="1">AUDI</option>
+                        <option value="2">LAND ROVER</option>
+                        <option value="3">PORSCHE</option>
+                    </select>
+                    <p className='m-1'>Modelo</p>
+                    <select class="form-select input w-75 border-1 border-dark rounded-0 mb-4" id="inputGroupSelect03">
+                        <option selected>Selecione um Modelo</option>
+                        <option value="1">718</option>
+                        <option value="2">911</option>
+                        <option value="3">BOXSTER</option>
+                        <option value="4">CAYENNE</option>
+                        <option value="5">CAYMAN</option>
+                        <option value="6">DEFENDER</option>
+                        <option value="7">MACAN</option>
+                        <option value="8">PANAMERA</option>
+                    </select>
+                    <p className='m-1'>Ordenação</p>
+                    <select class="form-select input w-75 border-1 border-dark rounded-0 mb-4" id="inputGroupSelect03">
+                        <option selected>Ordenação</option>
+                        <option value="1">Mais recentes</option>
+                        <option value="2">Menor preço</option>
+                        <option value="3">Maior preço</option>
+                        <option value="4">Menor Km</option>
+                        <option value="5">Maior Km</option>
+                    </select>
+                    <p className='m-1'>Ano</p>
+                    <section className='d-flex'>
+                    <select class="headerForm form-select w-25 input border-1 border-dark rounded-0" id="inputGroupSelect03">
+                        <option selected>de</option>
+                        <option value="1">Mais recentes</option>
+                        <option value="2">Menor preço</option>
+                        <option value="3">Maior preço</option>
+                        <option value="4">Menor Km</option>
+                        <option value="5">Maior Km</option>
+                    </select>
+                    <select class="headerForm form-select w-25 input border-1 border-dark rounded-0" id="inputGroupSelect03">
+                        <option selected>até</option>
+                        <option value="1">Mais recentes</option>
+                        <option value="2">Menor preço</option>
+                        <option value="3">Maior preço</option>
+                        <option value="4">Menor Km</option>
+                        <option value="5">Maior Km</option>
+                    </select>
+                    </section>
                 </section>
 
                 <section className="headerContentRight">{/* lado direito */}
